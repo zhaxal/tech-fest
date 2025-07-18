4bf5e36a50ee149c25c2df5721836737bc77fa5d
Anuarbek Zhakhangir <zhaxa65@gmail.com>
2025-07-18 16:32:58 +0900

    feat: add Dockerfile for Node.js application with multi-stage build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "standalone",
 };
 
 export default nextConfig;
