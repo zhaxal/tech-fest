29e7ed77b9ce8219a2bc542751776f89721a4685
Zhakhangir Anuarbek <zhaxa65@gmail.com>
2025-08-05 11:31:25 +0500

    Refactor code structure for improved readability and maintainability

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,11 +6,15 @@ function Map() {
     <div className="bg-[#272727] relative pt-14 pb-32">
       <div className="container mx-auto flex flex-col lg:flex-row px-4">
         <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0">
-          <h1 className={`${fluxgore.className} text-4xl md:text-5xl lg:text-7xl text-white relative`}>
+          <h1
+            className={`${fluxgore.className} text-4xl md:text-5xl lg:text-7xl text-white relative`}
+          >
             как нас найти
           </h1>
 
-          <h2 className={`${fluxgore.className} text-2xl md:text-3xl lg:text-4xl text-[#1068B0] mt-10 lg:mt-20`}>
+          <h2
+            className={`${fluxgore.className} text-2xl md:text-3xl lg:text-4xl text-[#1068B0] mt-10 lg:mt-20`}
+          >
             5-7 сентября 2025
           </h2>
 
@@ -27,7 +31,7 @@ function Map() {
 
         <div className="w-full lg:w-1/2">
           <iframe
-            src="https://yandex.com/map-widget/v1/?um=constructor%3Ad4445ea405fa54c9148340a7de87f7a4b9df164b35ff2fe504cf79adc1fab55c&amp;source=constructor"
+            src="https://yandex.com/map-widget/v1/?um=constructor%3A226a8e42b9987ad9a2b81d08d3da22c826d6209926040115001a9f9962fe7ea8&amp;source=constructor"
             className="w-full h-[300px] md:h-[400px] lg:h-[500px]"
             frameBorder="0"
           ></iframe>
